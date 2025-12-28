c0003be3710b14cc81228ceb72eeb97a8710888a
QuickerMaths <mateusz.szalowicz@gmail.com>
2025-12-28 15:03:59 +0100

    refactor: enhance fetchMovies

--- apps/web/api/movies.ts
+++ apps/web/api/movies.ts
@@ -3,11 +3,19 @@ import { IMovieGirdItem } from '@/types/movie';
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
 
 export async function fetchMovies(): Promise<IMovieGirdItem[]> {
-  const res = await fetch(`${API_URL}/movies`);
+  const url = new URL('/movies', API_URL);
+
+  const res = await fetch(url.toString());
 
   if (!res.ok) {
-    throw new Error(`Failed to fetch movies: ${res.statusText} (${res.status})`);
+    let errorMessage = `Error ${res.status}: ${res.statusText}`;
+    try {
+      const errorBody = await res.json();
+      errorMessage = errorBody.message || errorMessage;
+    } catch {}
+
+    throw new Error(errorMessage);
   }
 
-  return res.json();
+  return (await res.json()) as Promise<IMovieGirdItem[]>;
 }
