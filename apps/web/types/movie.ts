77116b0ef8741dbf97d7b3cdcb4818b4d25b022f
QuickerMaths <mateusz.szalowicz@gmail.com>
2025-12-28 14:49:20 +0100

    feat: create MovieGridItemEntity and update IMovie interface for improved movie data structure

--- apps/web/types/movie.ts
+++ apps/web/types/movie.ts
@@ -1,14 +1,10 @@
-export interface IMovie {
-  movie_id: number;
+export interface IMovieGirdItem {
+  id: number;
   title: string;
-  description: string | null;
-  poster_image_url: string | null;
-  cached_rating: number;
-  duration_minutes: number | null;
-  last_show_date: Date;
-  is_recommended: boolean;
-  movie_genres: {
-    genre_id: number;
+  posterImageUrl: string | null;
+  cachedRating: number;
+  durationMinutes: number | null;
+  movieGenres: {
     name: string;
   };
 }
