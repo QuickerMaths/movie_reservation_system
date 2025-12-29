0f10da135afc259c6a1018a77a71f1d081c25c16
QuickerMaths <mateusz.szalowicz@gmail.com>
2025-12-29 15:00:28 +0100

    feat: enhance MoviePage to include movie recommendations and prefetch recommended movies

--- apps/web/app/movies/[id]/page.tsx
+++ apps/web/app/movies/[id]/page.tsx
@@ -1,7 +1,8 @@
 import MovieDetails from '@/components/MovieDetails';
 import { getQueryClient } from '@/providers/get-query-client';
-import { fetchMovieDetailsById } from '@/api/movies';
+import { fetchMovieDetailsById, fetchRecommendedMovies } from '@/api/movies';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import MovieRecommendation from '@/components/MovieRecommendation';
 
 export default async function MoviePage({ params }: { params: { id: string } }) {
   const { id } = await params;
@@ -12,11 +13,19 @@ export default async function MoviePage({ params }: { params: { id: string } }) 
     queryFn: async () => await fetchMovieDetailsById(id),
   });
 
+  await queryClient.prefetchQuery({
+    queryKey: ['recommended-movies', id],
+    queryFn: async () => await fetchRecommendedMovies(id),
+  });
+
   return (
     <main className='min-h-screen bg-black text-white p-8 md:p-16'>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <MovieDetails id={id} />
-      </HydrationBoundary>
+      <div className='grid grid-cols-1 md:grid-cols-12 gap-8 mb-16'>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <MovieDetails id={id} />
+          <MovieRecommendation id={id} />
+        </HydrationBoundary>
+      </div>
     </main>
   );
 }
