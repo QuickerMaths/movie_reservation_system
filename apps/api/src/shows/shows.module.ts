7e4afa4d72fa210d42fa092d34a64727ecc39ef4
QuickerMaths <mateusz.szalowicz@gmail.com>
2025-12-30 16:40:49 +0100

    feat: add PrismaModule import to ShowsModule for database integration

--- apps/api/src/shows/shows.module.ts
+++ apps/api/src/shows/shows.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShowsService } from './shows.service';
 import { ShowsController } from './shows.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ShowsController],
   providers: [ShowsService],
 })
