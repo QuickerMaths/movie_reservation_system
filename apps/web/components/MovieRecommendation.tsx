4229e842200192a82e5882fb62674f91b0044bfe
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-01-03 10:31:47 +0100

    fix: use movie ID as key in MoviePreview component for recommended movies

--- apps/web/components/MovieRecommendation.tsx
+++ apps/web/components/MovieRecommendation.tsx
@@ -24,8 +24,8 @@ export default function MovieRecommendation({ id }: { id: string }) {
     <section className='md:col-start-1 md:col-end-12'>
       <h3 className='text-center text-xl font-semibold mb-8 text-gray-200'>Recommended movies</h3>
       <div className='grid gap-8'>
-        {recommendedMovies.map((movie, index) => (
-          <MoviePreview key={index} movie={movie} size={'sm'} />
+        {recommendedMovies.map((movie) => (
+          <MoviePreview key={movie.id} movie={movie} size={'sm'} />
         ))}
       </div>
     </section>
