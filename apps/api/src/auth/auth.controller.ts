62118365c6a37642a99ce4427a838c4b238d58fa
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-01-10 21:23:34 +0100

    feat(apps/api): add AuthController for user authentication endpoints

---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Res,
+  Body,
+  Get,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response, Request as ExpressRequest } from 'express';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UserEntity } from '../users/entities/user.entity';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { ApiOkResponse } from '@nestjs/swagger';
+
+interface RequestWithUser extends ExpressRequest {
+  user: UserEntity;
+}
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @ApiOkResponse({ type: UserEntity })
+  @UseInterceptors(ClassSerializerInterceptor)
+  login(
+    @Request() req: RequestWithUser,
+    @Res({ passthrough: true }) response: Response,
+  ): UserEntity {
+    const { access_token } = this.authService.login(req.user);
+
+    response.cookie('Authentication', access_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+    });
+
+    return req.user;
+  }
+
+  @Post('register')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ type: UserEntity })
+  async register(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+    return this.authService.register(createUserDto);
+  }
+
+  @Post('logout')
+  @ApiOkResponse({ type: Object })
+  logout(@Res({ passthrough: true }) response: Response): { message: string } {
+    response.clearCookie('Authentication');
+    return { message: 'Logged out' };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOkResponse({ type: UserEntity })
+  getProfile(@Request() req: RequestWithUser): UserEntity {
+    return req.user;
+  }
+}
