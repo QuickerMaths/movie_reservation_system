f631b0d1a35321a22aa2f7c370e783c454b0d25a
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-01-12 22:12:07 +0100

    feat(apps/api): enhance UserWithRoleEntity with role, phone number, newsletter opt-in, and preferred genre fields, using the class-transformer

--- apps/api/src/users/entities/user-with-role.entity.ts
+++ apps/api/src/users/entities/user-with-role.entity.ts
@@ -1,5 +1,6 @@
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude, Expose, Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { TUserWithRoles } from '../../../types/prisma/users-queries.types';
 
 export class UserWithRoleEntityEntity {
   @Expose({ name: 'id' })
@@ -18,20 +19,51 @@ export class UserWithRoleEntityEntity {
   @ApiProperty({ example: 'Doe' })
   last_name: string;
 
+  @Expose({ name: 'role' })
+  @Transform(({ obj }: { obj: TUserWithRoles }) => {
+    if (!obj.users_roles || obj.users_roles.length === 0) return null;
+
+    return obj.users_roles[0].roles.name;
+  })
   @ApiProperty({ example: 'regular' })
-  @Expose({ name: 'email' })
   role: string;
 
-  @ApiProperty({ example: '+1234567890' })
   @Expose({ name: 'phoneNumber' })
+  @Transform(({ obj }: { obj: TUserWithRoles }) => {
+    if (!obj.regular_user_profiles) return null;
+
+    return obj.regular_user_profiles.phone_number;
+  })
+  @ApiProperty({ example: '+1234567890' })
   phone_number: string | null;
 
-  @ApiProperty({ example: true })
   @Expose({ name: 'newsletterOptIn' })
+  @Transform(({ obj }: { obj: TUserWithRoles }) => {
+    if (!obj.regular_user_profiles) return null;
+
+    return obj.regular_user_profiles.newsletter_opt_in;
+  })
+  @ApiProperty({ example: true })
   newsletter_opt_in: boolean | null;
+
+  // TODO: change to genre object later
+  @Expose({ name: 'preferredGenreId' })
+  @Transform(({ obj }: { obj: TUserWithRoles }) => {
+    if (!obj.regular_user_profiles) return null;
+
+    return obj.regular_user_profiles.preferred_genre_id;
+  })
+  @ApiProperty({ example: 1 })
+  preferred_genre_id: number | null;
 
   @Exclude()
   password_hash: string;
+
+  @Exclude()
+  regular_user_profiles: object;
+
+  @Exclude()
+  users_roles: object;
 
   @Expose({ name: 'createdAt' })
   @ApiProperty()
