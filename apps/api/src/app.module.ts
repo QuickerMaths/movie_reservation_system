0852eba8174e228399e716c020cd33143a444293
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-01-12 10:08:18 +0100

    feat(apps/api): add AuthModule to the application module imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,19 +4,20 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 import { ShowsModule } from './shows/shows.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: '../../.env',
-      }),
-      MoviesModule,
-      ShowsModule,
-      UsersModule
-      ],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../../.env',
+    }),
+    MoviesModule,
+    ShowsModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
