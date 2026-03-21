fbf856292383e94b7e94659161c8617f132e26f6
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-03-21 22:38:40 +0100

    feat(api): enhance ReservationEntity with detailed ApiProperty descriptions

--- apps/api/src/reservations/entities/reservation.entity.ts
+++ apps/api/src/reservations/entities/reservation.entity.ts
@@ -4,31 +4,54 @@ import { ReservationStatus } from '../../../generated/prisma/client';
 
 export class ReservationEntity {
   @Expose({ name: 'id' })
-  @ApiProperty({ example: '1' })
+  @ApiProperty({ name: 'id', example: 1, description: 'Unique identifier of the reservation' })
   reservation_id: number;
 
   @Expose({ name: 'date' })
-  @ApiProperty({ example: '2024-03-20T10:00:00Z' })
+  @ApiProperty({
+    name: 'date',
+    example: '2024-03-20T10:00:00Z',
+    description: 'Reservation creation date',
+  })
   reservation_date: Date;
 
-  @Expose()
-  @ApiProperty({ enum: ReservationStatus, example: ReservationStatus.PENDING })
+  @Expose({ name: 'status' })
+  @ApiProperty({
+    enum: ReservationStatus,
+    example: ReservationStatus.PENDING,
+    description: 'Reservation status',
+  })
   status: ReservationStatus;
 
   @Expose({ name: 'userId' })
-  @ApiProperty({ example: 1 })
+  @ApiProperty({
+    name: 'userId',
+    example: 1,
+    description:
+      'Unique identifier of the user that made the reservation (this is optional for the guest users)',
+  })
   user_id: number;
 
   @Expose({ name: 'guestEmail' })
-  @ApiProperty({ example: 'john.doe@example.com' })
+  @ApiProperty({
+    name: 'guestEmail',
+    example: 'john.doe@example.com',
+    description:
+      'Email address used to send the confirmation email, if the users is not logged in.',
+  })
   guest_email: string;
 
   @Expose({ name: 'token' })
-  @ApiProperty({ example: '550e8400-e29b-41d4-a716-446655440000' })
+  @ApiProperty({
+    name: 'token',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    description:
+      'Token which allows canceling the reservation to the users that do not have the account.',
+  })
   cancellation_token: string;
 
   @Expose({ name: 'showId' })
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ name: 'showId', example: 1, description: 'Unique identifier of the show' })
   show_id: number;
 
   constructor(partial: Partial<ReservationEntity>) {
