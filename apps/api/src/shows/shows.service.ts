2f2a1d20f792d5c8a774f892708c375dd1bbee86
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-03-21 22:54:17 +0100

    feat(api): implement getShowSeats method to retrieve seat information with reservation status

--- apps/api/src/shows/shows.service.ts
+++ apps/api/src/shows/shows.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { shows } from '../../generated/prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { GetShowsDto } from './dto/get-shows-dto';
+import { NotFoundException } from '@nestjs/common';
+import { ReservationStatus } from '../../generated/prisma/client';
+import { SeatWithRelations } from './entities/seat-with-status.entity';
 
 @Injectable()
 export class ShowsService {
@@ -29,6 +32,42 @@ export class ShowsService {
       orderBy: {
         start_timestamp: 'asc',
       },
+    });
+  }
+
+  async getShowSeats(showId: number): Promise<SeatWithRelations[]> {
+    const TEN_MINUTES_AGO = new Date(Date.now() - 10 * 60 * 1000);
+
+    const show = await this.prisma.shows.findUnique({
+      where: { show_id: showId },
+      select: { movie_room_id: true },
+    });
+
+    if (!show) throw new NotFoundException('Show not found');
+
+    return this.prisma.seats.findMany({
+      where: { movie_room_id: show.movie_room_id },
+      include: {
+        seat_types: true,
+        tickets: {
+          where: {
+            show_id: showId,
+            reservations: {
+              OR: [
+                { status: ReservationStatus.PAID },
+                {
+                  status: ReservationStatus.PENDING,
+                  reservation_date: { gte: TEN_MINUTES_AGO },
+                },
+              ],
+            },
+          },
+          include: {
+            reservations: { select: { status: true } },
+          },
+        },
+      },
+      orderBy: [{ row_label: 'asc' }, { seat_number: 'asc' }],
     });
   }
 }
