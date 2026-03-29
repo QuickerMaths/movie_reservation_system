046631df947f708fa62893dbafc42c023e2fae20
QuickerMaths <mateusz.szalowicz@gmail.com>
2026-03-29 14:07:47 +0200

    feat(web): add useShowsSeatsIdQuery hook to fetch seat information for a specific show

--- apps/web/hooks/shows-query-hooks.ts
+++ apps/web/hooks/shows-query-hooks.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchShowsByMovieId } from '@/api/shows';
+import { fetchShowsByMovieId, fetchShowsSeatsById } from '@/api/shows';
 
 export function useShowsByMovieIdQuery(movieId: string, date: string) {
   return useQuery({
     queryKey: ['shows', movieId, date],
     queryFn: () => fetchShowsByMovieId(+movieId, date),
+    staleTime: 60 * 1000,
+  });
+}
+
+export function useShowsSeatsIdQuery({ showId }: { showId: string }) {
+  return useQuery({
+    queryKey: ['showsSeats', showId],
+    queryFn: () => fetchShowsSeatsById(showId),
     staleTime: 60 * 1000,
   });
 }
